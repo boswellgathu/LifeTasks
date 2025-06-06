bb9ef86fbadc6a5c6c5d7d624f79c6a943b219aa
boswellgathu <boswellgathu@gmail.com>
2025-06-06 21:09:28 +0300

    improve ux looks of the task list view

--- lib/views/tasks_list.dart
+++ lib/views/tasks_list.dart
@@ -1,14 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../controllers/task.dart';
 import '../controllers/category.dart';
-import '../models/category.dart';
-import 'create_category.dart';
 import 'create_task.dart';
 import 'task_details.dart';
-// Future screens:
-// import 'settings_screen.dart';
+import '../models/category.dart';
 
 class TaskListScreen extends StatefulWidget {
   const TaskListScreen({super.key});
@@ -36,7 +34,6 @@ class TaskListScreenState extends State<TaskListScreen> {
         actions: [
           IconButton(
             icon: Icon(Icons.add),
-            tooltip: 'New Task',
             onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(builder: (_) => TaskCreationScreen()),
@@ -45,13 +42,7 @@ class TaskListScreenState extends State<TaskListScreen> {
           ),
           PopupMenuButton<String>(
             onSelected: (value) {
-              if (value == 'settings') {
-                // TODO: Navigate to Settings Screen
-              } else if (value == 'add_category') {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (_) => CategoryCreationScreen()),
-                );
-              }
+              // Settings and Add Category will be wired here
             },
             itemBuilder: (BuildContext context) => [
               PopupMenuItem(
@@ -83,6 +74,7 @@ class TaskListScreenState extends State<TaskListScreen> {
           : taskController.tasks.isEmpty
           ? Center(child: Text('No tasks found. Create one!'))
           : ListView.builder(
+        padding: EdgeInsets.all(16),
         itemCount: taskController.tasks.length,
         itemBuilder: (context, index) {
           final task = taskController.tasks[index];
@@ -97,47 +89,96 @@ class TaskListScreenState extends State<TaskListScreen> {
             ),
           );
 
-          return Card(
-            child: ListTile(
-              title: Text(
-                task.title,
-                style: TextStyle(
-                  decoration: task.isDone
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => TaskDetailScreen(task: task),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 4,
+              margin: EdgeInsets.only(bottom: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Task image
+                    if (task.imagePath != null)
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.file(
+                          File(task.imagePath!),
+                          height: 80,
+                          width: 80,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    else
+                      Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade300,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Icon(Icons.image_not_supported, size: 40, color: Colors.grey),
+                      ),
+                    SizedBox(width: 16),
+                    // Task details
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            task.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          if (task.description != null && task.description!.isNotEmpty)
+                            Text(
+                              task.description!,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey[700],
+                              ),
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          SizedBox(height: 12),
+                          Row(
+                            children: [
+                              Chip(
+                                avatar: Icon(category.iconData, size: 16, color: Colors.white),
+                                label: Text(category.name),
+                                backgroundColor: Color(
+                                  int.parse(category.colorHex.replaceFirst('#', '0xff')),
+                                ),
+                                labelStyle: TextStyle(color: Colors.white),
+                                padding: EdgeInsets.symmetric(horizontal: 8),
+                                shape: StadiumBorder(),
+                              ),
+                              Spacer(),
+                              Icon(
+                                task.isDone ? Icons.check_circle : Icons.radio_button_unchecked,
+                                color: task.isDone ? Colors.green : Colors.grey,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (task.scheduledStart != null)
-                    Text(
-                      'Scheduled: ${task.scheduledStart!.toLocal()}',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 4.0),
-                    child: Chip(
-                      avatar: Icon(category.iconData, color: Colors.white, size: 16),
-                      label: Text(category.name),
-                      backgroundColor: Color(
-                        int.parse(category.colorHex.replaceFirst('#', '0xff')),
-                      ),
-                      labelStyle: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-              trailing: task.isDone
-                  ? Icon(Icons.check_circle, color: Colors.green)
-                  : Icon(Icons.radio_button_unchecked, color: Colors.grey),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => TaskDetailScreen(task: task),
-                  ),
-                );
-              },
             ),
           );
         },
